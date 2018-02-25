d584abb5265fcf05294aab0a67573a504d4e76a1
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-02-25 15:06:59 -0300

    Fim da aula 1, Single Responsability Principle

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -12,13 +12,13 @@ namespace SOLID
         static void Main(string[] args)
         {
             #region Aula 1
-            var funcionario = new Funcionario(new Dba(), 8000);
-            var funcionario2 = new Funcionario(new Desenvolvedor(), 5000);
+            //var funcionario = new Funcionario(new Dba(), 8000);
+            //var funcionario2 = new Funcionario(new Desenvolvedor(), 5000);
 
-            var calculadora = new CalculadoraSalario();
+            //var calculadora = new CalculadoraSalario();
 
-            Console.WriteLine(calculadora.Calcula(funcionario));
-            Console.WriteLine(calculadora.Calcula(funcionario2));
+            //Console.WriteLine(calculadora.Calcula(funcionario));
+            //Console.WriteLine(calculadora.Calcula(funcionario2));
 
             #endregion Aula 1
 
